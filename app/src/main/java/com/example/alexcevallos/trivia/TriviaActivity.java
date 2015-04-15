55d2189fb38dea4cb50fbf8bb8e133a39612677b
Alex Cevallos <cevalloa@gmail.com>
2015-04-14 22:30:39 -0400

    Added trivia answers, and functioning trivia randomizer

--- app/src/main/java/com/example/alexcevallos/trivia/TriviaActivity.java
+++ app/src/main/java/com/example/alexcevallos/trivia/TriviaActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 public class TriviaActivity extends Activity {
 
@@ -23,8 +25,31 @@ public class TriviaActivity extends Activity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+
+
                 //The button was clicked, update fact label with new fact
-                String fact = "Ostriches can run faster than horses";
+                String fact = "";
+
+                //Randomly select a fact
+                Random randomGenerator = new Random(); //Construct a new random generator
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+               fact = facts[randomNumber];
+
+                //Update the label with our dynamic fact
+
                 factLabel.setText(fact);
             }
         };
